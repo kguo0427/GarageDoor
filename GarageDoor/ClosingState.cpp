dc42d294ba66d1839df91b15005bb3b77e1acff7
mad1948 <mad1948@g.rit.edu>
2016-03-08 21:29:21 -0500

    Update ClosingState.cpp

--- GarageDoor/ClosingState.cpp
+++ GarageDoor/ClosingState.cpp
@@ -15,3 +15,23 @@ ClosingState::ClosingState() {
 ClosingState::~ClosingState() {
 	// TODO Auto-generated destructor stub
 }
+
+ClosingState::entry()
+{
+	// TODO
+	// motorDown = 1
+}
+
+ClosingState::exit()
+{
+	// TODO
+	// motorDown = 0
+}
+
+ClosingState::update()
+{
+	// TODO
+	// increment counter
+	// if counter == 10 then send fullDown signal
+	//			OR let someone else track counter (controller?)
+}
